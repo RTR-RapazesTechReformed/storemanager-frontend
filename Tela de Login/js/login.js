22cd33ab3e635c94688bf358cb887ab068d5be9e
YanCesario <yan.cesario@sptech.school>
2025-09-16 02:21:02 -0300

    Commitando de novo para resolver conflito do bash

--- Tela de Login/js/login.js
+++ Tela de Login/js/login.js
@@ -1,25 +1,25 @@
-document.getElementById('loginForm').addEventListener('submit', async function(event) {
+document.getElementById('loginForm').addEventListener('submit', async function (event) {
     event.preventDefault();
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    try{
+    try {
         const response = await fetch("http://localhost:8080/login", {
             method: 'POST',
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({username, password})
+            body: JSON.stringify({ username, password })
         });
-        if(response.ok){
+        if (response.ok) {
             window.location.href = "/Tela de Produtos/html/produtos.html";
-        }else{
+        } else {
             document.getElementById('errorMessage').textContent = "Usuário ou senha inválidos.";
             document.getElementById('errorMessage').style.display = 'block';
         }
     }
-    catch(error){
+    catch (error) {
         console.error("Erro no login:", error);
         document.getElementById('errorMessage').innerText = "Erro ao conectar ao servidor.";
         document.getElementById('errorMessage').style.display = 'block';
