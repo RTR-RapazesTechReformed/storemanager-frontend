ee6a33246886de86d19ecfb813165e012ca24c65
YanCesario <yan.cesario@sptech.school>
2025-09-16 02:14:31 -0300

    Subindo validação da tela de Login em JS

---
+++ Tela de Login/js/login.js
@@ -0,0 +1,27 @@
+document.getElementById('loginForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try{
+        const response = await fetch("http://localhost:8080/login", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({username, password})
+        });
+        if(response.ok){
+            window.location.href = "/Tela de Produtos/html/produtos.html";
+        }else{
+            document.getElementById('errorMessage').textContent = "Usuário ou senha inválidos.";
+            document.getElementById('errorMessage').style.display = 'block';
+        }
+    }
+    catch(error){
+        console.error("Erro no login:", error);
+        document.getElementById('errorMessage').innerText = "Erro ao conectar ao servidor.";
+        document.getElementById('errorMessage').style.display = 'block';
+    }
+});
