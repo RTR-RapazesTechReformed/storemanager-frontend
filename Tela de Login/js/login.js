b5393a6a11bbb18cff9670cafd4ab445769925f3
Enzo Victor Stanganelli Gama <64747700+Stanganelli@users.noreply.github.com>
2025-10-26 21:19:32 -0300

    Update login to use email instead of username

--- Tela de Login/js/login.js
+++ Tela de Login/js/login.js
@@ -1,40 +1,28 @@
 document.getElementById('loginForm').addEventListener('submit', async function (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
 
-    try {
-        const response = await fetch("http://localhost:8080/store-manager-api/auth/login", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ username, password })
-        });
-        if (response.ok) {
-              const data = await response.json();
+  try {
+    const response = await fetch("http://localhost:8080/store-manager-api/auth/login", {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password })
+    });
 
-    // Extrai os campos necessários
-    const sessionId = data.session_id;
-    const userId = data.user_id;
-
-    // Armazena no sessionStorage
-    sessionStorage.setItem("session-id", sessionId);
-    sessionStorage.setItem("user-id", userId);
-
-    // Redireciona para a página inicial
-
-    window.location.href = "Tela%home/html/estoque.html";
-
-        } else {
-            document.getElementById('errorMessage').textContent = "Usuário ou senha inválidos.";
-            document.getElementById('errorMessage').style.display = 'block';
-        }
+    if (response.ok) {
+      const data = await response.json();
+      sessionStorage.setItem("session-id", data.session_id);
+      sessionStorage.setItem("user-id", data.user_id);
+      window.location.href = "../../Tela home/html/estoque.html";
+    } else {
+      document.getElementById('errorMessage').textContent = "Usuário ou senha inválidos.";
+      document.getElementById('errorMessage').style.display = 'block';
     }
-    catch (error) {
-        console.error("Erro no login:", error);
-        document.getElementById('errorMessage').innerText = "Erro ao conectar ao servidor.";
-        document.getElementById('errorMessage').style.display = 'block';
-    }
+  } catch (error) {
+    console.error("Erro no login:", error);
+    document.getElementById('errorMessage').innerText = "Erro ao conectar ao servidor.";
+    document.getElementById('errorMessage').style.display = 'block';
+  }
 });
